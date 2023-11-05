02c53b9bfe67b330d9891efa7346623fe57cf2b7
tiffany xiang <tiffany@macbook-pro.lan>
2023-11-05 01:36:35 -0500

    mapping projectsList instead

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,8 +1,58 @@
 import ProjectItem from "../Components/ProjectItem";
 import { motion } from "framer-motion";
-import { useState } from "react";
 
 const Portfolio = () => {
+  const projectsList = [
+    {
+      projectTitle: "Spring Day",
+      projectType: "Original Work",
+      fileName: "./cover-art.png",
+      fileNameMin: "./cover-art-min.png",
+    },
+    {
+      projectTitle: "Paper Cranes",
+      projectType: "Original Work",
+      fileName: "./girl4.PNG",
+      fileNameMin: "./girl4-min.PNG",
+    },
+    {
+      projectTitle: "Venti",
+      projectType: "Fanart/Commission",
+      fileName: "./venti.PNG",
+      fileNameMin: "./venti-min.PNG",
+    },
+    {
+      projectTitle: "Miya Twins",
+      projectType: "Fanart",
+      fileName: "./guys.PNG",
+      fileNameMin: "./guys-min.PNG",
+    },
+    {
+      projectTitle: "Kokomi",
+      projectType: "Fanart",
+      fileName: "./kokomi.png",
+      fileNameMin: "./kokomi-min.png",
+    },
+    {
+      projectTitle: "Hanni",
+      projectType: "Fanart",
+      fileName: "./girl3.jpg",
+      fileNameMin: "./girl3-min.jpg",
+    },
+    {
+      projectTitle: "Orange Justice",
+      projectType: "Draft",
+      fileName: "./girl1.PNG",
+      fileNameMin: "./girl1-min.PNG",
+    },
+    {
+      projectTitle: "Butterfly",
+      projectType: "Draft",
+      fileName: "./girl2.PNG",
+      fileNameMin: "./girl2-min.PNG",
+    },
+  ];
+
   return (
     <motion.div
       initial={{
@@ -16,54 +66,15 @@ const Portfolio = () => {
       className="flex items-start self-center justify-start h-full py-5 overflow-scroll text-center lg:py-10 lg:w-1/2 lg:text-start lg:self-end no-scrollbar"
     >
       <ul className="flex flex-col items-center lg:items-start lg:pl-14">
-        <ProjectItem
-          projectTitle="Project Name"
-          projectType="Project Type"
-          fileName="./cover-art.png"
-          fileNameMin="./cover-art-min.png"
-        />
-        <ProjectItem
-          projectTitle="Project Name"
-          projectType="Project Type"
-          fileName="./kokomi.png"
-          fileNameMin="./kokomi-min.png"
-        />
-        <ProjectItem
-          projectTitle="Project Name"
-          projectType="Project Type"
-          fileName="./girl1.PNG"
-          fileNameMin="./girl1-min.PNG"
-        />
-        <ProjectItem
-          projectTitle="Project Name"
-          projectType="Project Type"
-          fileName="./girl2.PNG"
-          fileNameMin="./girl2-min.PNG"
-        />
-        <ProjectItem
-          projectTitle="Project Name"
-          projectType="Project Type"
-          fileName="./girl3.jpg"
-          fileNameMin="./girl3-min.jpg"
-        />
-        <ProjectItem
-          projectTitle="Project Name"
-          projectType="Project Type"
-          fileName="./girl4.PNG"
-          fileNameMin="./girl4-min.PNG"
-        />
-        <ProjectItem
-          projectTitle="Project Name"
-          projectType="Project Type"
-          fileName="./venti.PNG"
-          fileNameMin="./venti-min.PNG"
-        />
-        <ProjectItem
-          projectTitle="Project Name"
-          projectType="Project Type"
-          fileName="./guys.PNG"
-          fileNameMin="./guys-min.PNG"
-        />
+        {projectsList.map((project, index) => (
+          <ProjectItem
+            key={index}
+            projectTitle={project.projectTitle}
+            projectType={project.projectType}
+            fileName={project.fileName}
+            fileNameMin={project.fileNameMin}
+          />
+        ))}
       </ul>
     </motion.div>
   );
