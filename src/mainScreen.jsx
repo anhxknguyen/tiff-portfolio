d7cccf11af79536b038fa162907d7685666e964b
tiffany xiang <tiffany@macbook-pro.lan>
2023-11-05 03:55:45 -0500

    trial and error 3

--- src/mainScreen.jsx
+++ src/mainScreen.jsx
@@ -29,7 +29,7 @@ function MainScreen() {
         transition: { delay: 0, duration: 0.5 },
       }}
       exit={{ opacity: 0, transition: { duration: 0.25 } }}
-      className="flex flex-col items-start h-full bg-cover font-textFont text-textColor bg-lineartTempBg"
+      className="flex flex-col items-start h-full bg-cover font-textFont text-textColor lineartTempBg"
     >
       <UserContext.Provider
         value={{ project: project, setProject: setProject }}
