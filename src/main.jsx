69969c8f0729f2821ad8ec860f2dd84c933608c2
tiffany xiang <tiffany@macbook-pro.lan>
2023-11-06 23:36:34 -0500

    test

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { HashRouter as BrowserRouter } from "react-router-dom";
 
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
-window.addEventListener("resize", () => {
+
+screen.orientation.addEventListener("change", (event) => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
