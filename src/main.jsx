fd6b4614ad37dd9b7548cb20ea2ffde55ab34d50
tiffany xiang <tiffany@macbook-pro.lan>
2023-11-06 20:08:48 -0500

    removed dynamic window height

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import "./styles/styles.css";
 import MainScreen from "./mainScreen.jsx";
 import { HashRouter as BrowserRouter } from "react-router-dom";
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+// let vh = window.innerHeight * 0.01;
+// document.documentElement.style.setProperty("--vh", `${vh}px`);
+// window.addEventListener("resize", () => {
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty("--vh", `${vh}px`);
+// });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
