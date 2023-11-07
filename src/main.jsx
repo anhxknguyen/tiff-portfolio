26339284fb598309d8e5e9b7f89b38f26dce5884
tiffany xiang <tiffany@macbook-pro.lan>
2023-11-06 20:26:21 -0500

    reverted changes

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ let vh = window.innerHeight * 0.01;
 let onloadWidth = window.innerWidth;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 window.addEventListener("resize", () => {
-  if (onloadWidth === window.innerWidth) return;
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
