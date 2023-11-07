e753743ed820a116fed09f7b53df288e1cbdd81f
tiffany xiang <tiffany@macbook-pro.lan>
2023-11-06 20:35:57 -0500

    trial and error zoom 5

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,17 @@ import MainScreen from "./mainScreen.jsx";
 import { HashRouter as BrowserRouter } from "react-router-dom";
 
 let vh = window.innerHeight * 0.01;
-let onloadWidth = window.innerWidth;
+let onloadWidth = 0;
+window.addEventListener("load", (event) => {
+  onloadWidth = window.innerWidth;
+});
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  if (window.innerWidth != onloadWidth) {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    onloadWidth = window.innerWidth;
+  }
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
